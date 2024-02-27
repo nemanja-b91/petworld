f2a5e4518be03429e246cb3c3bd4d08944a023ad
Bacha <nemanja91.bacic@gmail.com>
2024-02-27 23:33:29 +0100

    ADD - Added speed insights.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import '../assets/libraries/font-awesome.min.css';
 import './globals.scss'
 import Footer from "@/components/Footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function RootLayout({
   children,
@@ -13,6 +15,7 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en">
+      <SpeedInsights/>
       <body className={inter.className}>
       <NavBar/>
       <main>
