1cfeb4f5982c314728c27f51ca2cde70fd500489
Bacha <nemanja91.bacic@gmail.com>
2024-03-04 00:48:24 +0100

    MOD - Modified app, added new images.

--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -3,7 +3,6 @@ import SinglePageHero from "@/components/SinglePageHero/SinglePageHero";
 import {openGraphImage} from "@/app/shared-metadata";
 import FilterComponent from "@/components/Filter/Filter";
 import {Suspense} from "react";
-import Image from "next/legacy/image";
 const pageName: String = 'Restaurants';
 
 export const metadata = {
@@ -26,30 +25,16 @@ export default function RestaurantsPage() {
         <div className={styles.main}>
             <div className="container">
                 <SinglePageHero name={pageName}/>
-                <div className="row">
-                    <div className="col-md-4">
-                        <Image src={'/assets/img/formatted/pet-in-restaurant.jpg'}
-                               width={400}
-                               height={400}
-                               priority={false}
-                               alt={''}
-                               loading={'lazy'}
-                               style={{borderRadius: '10px'}}
-                        />
-                    </div>
-                    <div className="col-md-8">
-                        <article>
-                            <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-                                PageMaker
-                                including versions of Lorem Ipsum.</p>
-                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                                Richard
-                                McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going
-                                through
-                                the cites of the word in classical literature, discovered the undoubtable source.</p>
-                        </article>
-                    </div>
-                </div>
+                <article>
+                    <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                        PageMaker
+                        including versions of Lorem Ipsum.</p>
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+                        Richard
+                        McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going
+                        through
+                        the cites of the word in classical literature, discovered the undoubtable source.</p>
+                </article>
 
                 <Suspense>
                     <FilterComponent/>
--- app/saloons/page.tsx
+++ app/saloons/page.tsx
@@ -29,31 +29,16 @@ export default function SaloonsPage() {
         <div className={styles.main}>
             <div className="container">
                 <SinglePageHero name={pageName}/>
-                <div className="row">
-                    <div className="col-md-4">
-                        <Image src={'/assets/img/formatted/pet-in-grooming-saloon.jpg'}
-                               width={400}
-                               height={400}
-                               priority={false}
-                               alt={''}
-                               loading={'lazy'}
-                               style={{borderRadius: '10px'}}
-                        />
-                    </div>
-                    <div className="col-md-8">
-                        <article>
-                            <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-                                PageMaker
-                                including versions of Lorem Ipsum.</p>
-                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                                Richard
-                                McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going
-                                through
-                                the cites of the word in classical literature, discovered the undoubtable source.</p>
-
-                        </article>
-                    </div>
-                </div>
+                <article>
+                    <p> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+                        PageMaker
+                        including versions of Lorem Ipsum.</p>
+                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+                        Richard
+                        McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going
+                        through
+                        the cites of the word in classical literature, discovered the undoubtable source.</p>
+                </article>
                 <Suspense fallback={<p>Loading data...</p>}>
                     <FilterComponent/>
                 </Suspense>
